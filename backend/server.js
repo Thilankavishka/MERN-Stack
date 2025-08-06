6cead20f849f24a1836a1a06fa17411eb8be3edc
Thilankavishka <thilankavishka25@gmail.com>
2025-08-06 05:44:29 +0530

    create api

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,14 @@
-console.log("Hello World");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port  = process.env.PORT || 5000
 
+const app = express();
+
+app.listen(port,() => console.log(`server started on port ${port}`))
+
+
+app.get("/api/message",(req,res) => {
+
+   res.json({message: 'helloo i am the message route'})
+
+})
